b6bc20e186281070a773939bc3688f4f86bda430
LurieK <Lurainekimmerle@gmail.com>
2023-04-27 16:50:33 +0800

    set up api

--- tarotmvp.js
+++ tarotmvp.js
@@ -93,7 +93,11 @@ const cardChoice = document.querySelector(".card-choice");
 button.addEventListener('click', function (){
     let randomIndex = Math.floor(Math.random()*cards.length);
     let randomCard = cards[randomIndex];
+    const cardName = randomCard.Name
+    console.log(cardName)
     cardChoice.innerHTML = `<h3>${randomCard.Name}</h3><img src="${randomCard.img}" alt="${randomCard.Name}" style="display: block;"><h4>${randomCard.text}<h4>`
+    getResponse(cardName);
+
 });
 
 
@@ -344,88 +348,34 @@ dropDown.addEventListener('change', function (){
             cardChoice.innerHTML = `<h3>${cards[77].Name}</h3><img src="${cards[77].img}" alt="${cards[77].Name}" style="display: block;"><h4>${cards[77].text}<h4>`;
             break;
     }
-
+    getResponse(selectValue)
 })
 
 
 
-// const fool = document.querySelector('option[value="fool"]');
-// const magician = document.querySelector('option[value="magician"]');
-// const priestess = document.querySelector('option[value="priestess"]');
-// const empress = document.querySelector('option[value="empress"]');
-// const emperor = document.querySelector('option[value="emperor"]');
-// const hierophant = document.querySelector('option[value="heirophant"]');
-// const lovers = document.querySelector('option[value="lovers"]');
-// const chariot = document.querySelector('option[value="chariot"]');
-// const strength = document.querySelector('option[value="strength"]');
-// const hermit = document.querySelector('option[value="hermit"]');
-// const wheel = document.querySelector('option[value="wheel"]');
-// const justice = document.querySelector('option[value="justice"]');
-// const hanged = document.querySelector('option[value="hanged"]');
-// const death = document.querySelector('option[value="death"]');
-// const devil = document.querySelector('option[value="devil"]');
-// const tower = document.querySelector('option[value="tower"]');
-// const star = document.querySelector('option[value="star"]');
-// const moon = document.querySelector('option[value="moon"]');
-// const sun = document.querySelector('option[value="sun"]');
-// const judgement = document.querySelector('option[value="judgement"]');
-// const world = document.querySelector('option[value="world"]');
-// const as = document.querySelector('option[value="as"]');
-// const _2s = document.querySelector('option[value="2s"]');
-// const _3s = document.querySelector('option[value="3s"]');
-// const _4s = document.querySelector('option[value="4s"]');
-// const _5s = document.querySelector('option[value="5s"]');
-// const _6s = document.querySelector('option[value="6s"]');
-// const _7s = document.querySelector('option[value="7s"]');
-// const _8s = document.querySelector('option[value="8s"]');
-// const _9s = document.querySelector('option[value="9s"]');
-// const _10s = document.querySelector('option[value="10s"]');
-// const ps = document.querySelector('option[value="ps"]');
-// const kns = document.querySelector('option[value="kns"]');
-// const qs = document.querySelector('option[value="qs"]');
-// const ks = document.querySelector('option[value="ks"]');
-// const aw = document.querySelector('option[value="aw"]');
-// const _2w = document.querySelector('option[value="2w"]');
-// const _3w = document.querySelector('option[value="3w"]');
-// const _4w = document.querySelector('option[value="4w"]');
-// const _5w = document.querySelector('option[value="5w"]');
-// const _6w = document.querySelector('option[value="6w"]');
-// const _7w = document.querySelector('option[value="7w"]');
-// const _8w = document.querySelector('option[value="8w"]');
-// const _9w = document.querySelector('option[value="9w"]');
-// const _10w = document.querySelector('option[value="10w"]');
-// const pw = document.querySelector('option[value="pw"]');
-// const knw = document.querySelector('option[value="knw"]');
-// const qw = document.querySelector('option[value="qw"]');
-// const kw = document.querySelector('option[value="kw"]');
-// const ap = document.querySelector('option[value="ap"]');
-// const _2p = document.querySelector('option[value="2p"]');
-// const _3p = document.querySelector('option[value="3p"]');
-// const _4p = document.querySelector('option[value="4p"]');
-// const _5p = document.querySelector('option[value="5p"]');
-// const _6p = document.querySelector('option[value="6p"]');
-// const _7p = document.querySelector('option[value="7p"]');
-// const _8p = document.querySelector('option[value="8p"]');
-// const _9p = document.querySelector('option[value="9p"]');
-// const _10p = document.querySelector('option[value="10p"]');
-// const pp = document.querySelector('option[value="pp"]');
-// const knp = document.querySelector('option[value="knp"]');
-// const qp = document.querySelector('option[value="qp"]');
-// const kp = document.querySelector('option[value="kp"]');
-// const ac = document.querySelector('option[value="ac"]');
-// const _2c = document.querySelector('option[value="2c"]');
-// const _3c = document.querySelector('option[value="3c"]');
-// const _4c = document.querySelector('option[value="4c"]');
-// const _5c = document.querySelector('option[value="5c"]');
-// const _6c = document.querySelector('option[value="6c"]');
-// const _7c = document.querySelector('option[value="7c"]');
-// const _8c = document.querySelector('option[value="8c"]');
-// const _9c = document.querySelector('option[value="9c"]');
-// const _10c = document.querySelector('option[value="10c"]');
-// const pc = document.querySelector('option[value="pc"]');
-// const knc = document.querySelector('option[value="knc"]');
-// const qc = document.querySelector('option[value="qc"]');
-// const kc = document.querySelector('option[value="kc"]');
 
+async function getResponse(card){
+const url = 'https://chatgpt-ai-chat-bot.p.rapidapi.com/ask';
+const options = {
+	method: 'POST',
+	headers: {
+		'content-type': 'application/json',
+		'X-RapidAPI-Key': '526d114da5mshd11026488d6ba9dp15cf91jsn42c0b1394840',
+		'X-RapidAPI-Host': 'chatgpt-ai-chat-bot.p.rapidapi.com'
+	},
+	body: JSON.stringify({
+		query: `You are a expert Tarot reader.  I will give you a card and you will tell me what it means as a prediction
+        for the day. The card is the ${card}.`
+	})
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
+}
 
 
