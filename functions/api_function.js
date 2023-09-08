b1c3ed2b368c6423ecbf0e78e3a4799a0fddd0c8
LurieK <LuraineKimmerle@gmail.com>
2023-09-08 17:58:32 +0800

    create netlify function from old server.mjs.
    server.mjs will remain commented out for now

---
+++ functions/api_function.js
@@ -0,0 +1,39 @@
+import fetch from 'node-fetch';
+import { config } from 'dotenv';
+
+config();
+
+exports.handler = async function(event, context) {
+  const { card } = event.queryStringParameters;
+  const API_KEY = process.env.API_KEY;
+  const url = "https://chatgpt-ai-chat-bot.p.rapidapi.com/ask";
+
+  const options = {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      "X-RapidAPI-Key": API_KEY,
+      "X-RapidAPI-Host": "chatgpt-ai-chat-bot.p.rapidapi.com",
+    },
+    body: JSON.stringify({
+      query: `You are an expert Tarot reader. I will give you a card and you will tell me what it means as a prediction for the day. Please avoid negative readings and aim to inspire me with the meaning of the card. The card is the ${card}.`,
+    }),
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*", // Update this to match your front-end URL in production
+      },
+      body: JSON.stringify(result),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
