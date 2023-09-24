7e75bbe291e6a86aa122fc33d83658a897217ce0
LurieK <LuraineKimmerle@gmail.com>
2023-09-24 14:31:17 +0800

    update if else to use  netlify

--- script.js
+++ script.js
@@ -35,11 +35,11 @@ dropDown.addEventListener("change", function () {
 });
 
 async function getResponse(card) {
-  let url 
+  let url = 
   if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
     url = `http://localhost:5500/tarot?card=${card}`;
   } else {
-    url = `https://daily-tarot.vercel.app/tarot?card=${card}`;
+    url = `https://your-netlify-app.netlify.app/tarot?card=${card}`;
   }  console.log("Fetching URL: ", url); 
 
 
