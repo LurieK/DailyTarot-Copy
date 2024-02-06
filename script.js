8aa7b1722c832a7298ee9aa934da0bc3065dfc5c
Lurie Kimmerle <LuraineKimmerle@gmail.com>
2024-02-06 17:03:41 +0800

    update script.js endpoints to run locally

--- script.js
+++ script.js
@@ -42,9 +42,9 @@ dropDown.addEventListener("change", function () {
 async function getResponse(card) {
   let url 
   if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
-    url = `http://localhost:5500/tarot?card=${card}`;
+    url = `http://localhost:5500/.netlify/functions/api_function?card=${card}`;
   } else {
-    url = `https://famous-pithivier-c2c9b0.netlify.app/.netlify/functions/api_function?card=${card}`;
+    url = `https://famous-pithivier-c2c9b0.netlify.app/.netlify/functions/api_functions?card=${card}`;
   }  console.log("Fetching URL: ", url); 
 
 
